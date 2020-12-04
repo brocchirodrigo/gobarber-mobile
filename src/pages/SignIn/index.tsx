3e10140290a310605da27b00c1cb99ceb98320ab
Rodrigo Brocchi <rodrigobrocchi@MacBook-Pro-de-Rodrigo-Brocchi.local>
2020-12-04 01:41:47 -0300

    Ajuste de componentes e Signin

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title } from './styles';
+
+import logoImg from '../../assets/Logo/logo.png';
 
 export const SignIn: React.FC = () => {
   return (
     <>
-      <Container />
-      <View />
+      <Container>
+        <Image source={logoImg} />
+
+        <Title>Faça seu logon</Title>
+        <Input name="email" icon="mail" placeholder="E-mail" />
+        <Input name="password" icon="lock" placeholder="Senha" />
+
+        <Button
+          onPress={() => {
+            console.log('1');
+          }}
+        >
+          Entrar
+        </Button>
+      </Container>
     </>
   );
 };
