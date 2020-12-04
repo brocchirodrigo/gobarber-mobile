df6b3049b5308cfa9bdf348b6b4469c49e52a8d5
Rodrigo Brocchi <rodrigobrocchi@MacBook-Pro-de-Rodrigo-Brocchi.local>
2020-12-04 00:10:17 -0300

    Rotas de cadastro e entrada

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+export const SignUp: React.FC = () => {
+  return (
+    <>
+      <Container />
+      <View />
+    </>
+  );
+};
