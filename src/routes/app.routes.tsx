578f60fb85c3d72cb8c45f1cf1a54cddf91c0eaf
Rodrigo Brocchi <rodrigobrocchi@MacBook-Pro-de-Rodrigo-Brocchi.local>
2020-12-06 01:34:36 -0300

    Auth e teste signOut

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,28 @@
+// Módulos
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Importação de páginas
+
+import Dashboard from '../pages/Dashboard';
+
+// Instâncias
+
+const App = createStackNavigator();
+
+const AppRoutes: React.FC = () => {
+  return (
+    <>
+      <App.Navigator
+        screenOptions={{
+          // headerShown: false,
+          cardStyle: { backgroundColor: '#312e38' },
+        }}
+      >
+        <App.Screen name="Dashboard" component={Dashboard} />
+      </App.Navigator>
+    </>
+  );
+};
+
+export default AppRoutes;
